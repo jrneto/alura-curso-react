ad3b21da209a940017d03e11924e0b9ca618afb0
jrneto <reato.neto@gmail.com>
2026-02-11 08:47:01 -0300

    criação dos componentes banner e tema

--- tecboard/src/App.jsx
+++ tecboard/src/App.jsx
@@ -1,20 +1,46 @@
 
 import './App.css'
+import { Banner } from './componentes/Banner'
 import { FormularioDeEvento } from './componentes/FormularioDeEvento'
+import { Tema } from './componentes/Tema'
 // no react, componentes são funções
 
-
 function App() {
+
+  const temas = [
+    { id: 1, nome: 'front-end' },
+    { id: 2, nome: 'back-end' },
+    { id: 3, nome: 'devops' },
+    { id: 4, nome: 'inteligência artificial' },
+    { id: 5, nome: 'data science' },
+    { id: 6, nome: 'cloud' }
+  ]
 
   return (
     <main>
       <header>
         <img src="/logo.png"></img>
       </header>
-      <section>
-        <img src="/banner.png" alt=""></img>
-      </section>
+      <Banner />
       <FormularioDeEvento />
+      <section>
+        <Tema tema={temas[0]}/>
+      </section>
+      <section>
+        <Tema tema={temas[1]}/>
+      </section>
+      <section>
+        <Tema tema={temas[2]}/>
+      </section>
+      <section>
+        <Tema tema={temas[3]}/>
+      </section>
+      <section>
+        <Tema tema={temas[4]}/>
+      </section>
+      <section>
+        <Tema tema={temas[5]}/>
+      </section>
     </main>
   )
 }
