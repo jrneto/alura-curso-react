58792ae7e741e3d005e3541623c6df3c2f2364c0
jrneto <reato.neto@gmail.com>
2026-02-20 07:57:03 -0300

    finalização do formulario de eventos

--- tecboard/src/App.jsx
+++ tecboard/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { Banner } from './componentes/Banner'
 import { FormularioDeEvento } from './componentes/FormularioDeEvento'
 import { Tema } from './componentes/Tema'
+import { CardEvento } from './componentes/CardEvento'
+import { useState } from 'react';
 // no react, componentes são funções
 
 function App() {
@@ -16,31 +18,46 @@ function App() {
     { id: 6, nome: 'cloud' }
   ]
 
+
+  const [eventos, setEventos] = useState(
+    [
+      {
+        capa: '/evento-mulheres-no-front.png',
+        tema: temas[0],
+        data: new Date(),
+        titulo: 'Mulheres no Front'
+      }
+    ]
+  )
+
+  function adicionarEvento(evento) {
+      // eventos.push(evento);
+      // console.log('Eventos =>', eventos)
+      //Cria um novo array copiando todos os eventos atuais e adicionando o novo evento no final.
+      setEventos([...eventos, evento])
+  }
+
   return (
     <main>
       <header>
         <img src="/logo.png"></img>
       </header>
       <Banner />
-      <FormularioDeEvento />
-      <section>
-        <Tema tema={temas[0]}/>
-      </section>
-      <section>
-        <Tema tema={temas[1]}/>
-      </section>
-      <section>
-        <Tema tema={temas[2]}/>
-      </section>
-      <section>
-        <Tema tema={temas[3]}/>
-      </section>
-      <section>
-        <Tema tema={temas[4]}/>
-      </section>
-      <section>
-        <Tema tema={temas[5]}/>
-      </section>
+      <FormularioDeEvento 
+        temas={temas} 
+        aoSubmeter={adicionarEvento}/>
+      {temas.map(function (item) {
+        return (
+          <section key={item.id}>
+            <Tema tema={item} />
+            {eventos.map(function (item, indice) {
+
+              return <CardEvento evento={item} key={indice}/>
+            })}
+          </section>
+        )
+      })}
+      
     </main>
   )
 }
--- tecboard/src/componentes/FormularioDeEvento/index.jsx
+++ tecboard/src/componentes/FormularioDeEvento/index.jsx
@@ -6,9 +6,24 @@ import { TituloFormulario } from "../TituloFormulario"
 import { Botao } from '../Botao'
 import { ListaSuspensa } from '../ListaSuspensa'
 
-export function FormularioDeEvento() {
+export function FormularioDeEvento({ temas, aoSubmeter }) {
+
+
+  function aoFormSubmetido(formData) {
+    console.log('Formulário submetido', formData)
+    const evento = {
+      capa: formData.get('capa'),
+      tema: temas.find(function (item) {
+        return item.id == formData.get('tema')
+      }),
+      data: new Date(formData.get('dataEvento')),
+      titulo: formData.get('nomeEvento')
+    }
+    aoSubmeter(evento)
+  }
+
   return (
-    <form className="form-evento">
+    <form className="form-evento" action={aoFormSubmetido}>
       <TituloFormulario>
         Preencha para criar um evento
       </TituloFormulario>
@@ -17,15 +32,36 @@ export function FormularioDeEvento() {
           <Label htmlFor="nomeEvento">
             Qual o nome do evento?
           </Label>
-          <CampoDeEntrada type="text" id="nome" placeholder='Summer dev hits'></CampoDeEntrada>
+          <CampoDeEntrada
+            type="text"
+            id="nomeEvento"
+            placeholder='Summer dev hits'
+            name="nomeEvento"></CampoDeEntrada>
+        </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor="capa">
+            Qual o endereço da imagem de capa?
+          </Label>
+          <CampoDeEntrada
+            type="text"
+            id="capa"
+            placeholder='http://...'
+            name="capa"></CampoDeEntrada>
         </CampoDeFormulario>
         <CampoDeFormulario>
           <Label htmlFor="dataEvento">
             Data do evento
           </Label>
-          <CampoDeEntrada type="date" id="dataEvento"></CampoDeEntrada>
-          <ListaSuspensa />
-        </CampoDeFormulario>        
+          <CampoDeEntrada 
+            type="date" id="dataEvento" name="dataEvento">
+          </CampoDeEntrada>
+        </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor="tema">
+            Tema do evento
+          </Label>
+          <ListaSuspensa id="tema" name="tema" itens={temas} />
+        </CampoDeFormulario>
       </div>
       <div className='acoes'>
         <Botao>Criar evento</Botao>
